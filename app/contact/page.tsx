a884880a2c218f883a0f95ff249a5013780a7330
Muhammad Aryan <160228293+MuhammadAriyan@users.noreply.github.com>
2024-11-10 00:35:37 +0500

    Update page.tsx

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import '@/app/contact/contact.css'
 import Link from 'next/link'
-import { AtSign, GithubIcon, InstagramIcon, Linkedin, Mail, YoutubeIcon } from 'lucide-react'
+import { AtSign, GithubIcon, InstagramIcon, Linkedin, YoutubeIcon } from 'lucide-react'
 const Page = () => {
   return (
     <div>
       <h1 className='contact-heading top-section'>CONNECT WITH ME</h1>
-      <p className='contact-para top-section'>I’d love to hear from you! Whether you have a project in mind, want to collaborate, or simply want to chat, feel free to reach out. Let's explore new possibilities and create something amazing together. Drop me a message below, and let’s begin our journey!</p>
+      <p className='contact-para top-section'>I’d love to hear from you! Whether you have a project in mind, want to collaborate, or simply want to chat, feel free to reach out. Let’s explore new possibilities and create something amazing together. Drop me a message below, and let’s begin our journey!</p>
       <div className='social-icon-set top-section'>
         <Link href={'https://www.instagram.com/maryanrar'} target="_blank"><span className='social-icon' ><InstagramIcon/> Instagram</span></Link>
         <br />
